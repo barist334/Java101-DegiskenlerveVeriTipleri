d806034c30e175cf52ece29c59e265dd37210251
Barış Temiz <35572729+barist334@users.noreply.github.com>
2021-12-08 21:28:06 +0300

    Add files via upload

---
+++ src/Odevler_Pratikler/UcgenAlanHesabi.java
@@ -0,0 +1,23 @@
+package Odevler_Pratikler;
+
+import java.util.Scanner;
+
+public class UcgenAlanHesabi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double u, alan;
+        int a,b,c;
+        System.out.print("Birinci Kenarı Giriniz: ");
+        a = scanner.nextInt();
+        System.out.print("İkinci Kenarı Giriniz: ");
+        b = scanner.nextInt();
+        System.out.print("Üçüncü Kenarı Giriniz: ");
+        c = scanner.nextInt();
+
+        u = (a + b + c) / 2.0;
+        // Alan * Alan = 𝑢 * (𝑢 − 𝑎)* (𝑢 − 𝑏) * (𝑢 − 𝑐)
+        alan = Math.sqrt((u * (u-a) * (u-b) * (u-c)));
+        System.out.println("Üçgenin Alanı: " + alan);
+
+    }
+}
